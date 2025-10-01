796cfb60f503d077ee1e856a8d7a27cc3e3e8586
Your Name <albatintotaj4@gmail.com>
2025-10-01 16:48:48 +0200

    Created components folder and UserList file

---
+++ src/components/UserList.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (loading) return <p>Loading users...</p>;
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.company.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserList;
