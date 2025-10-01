f57593f53b76d6942d6544b2b357599f1fe94e9c
Your Name <albatintotaj4@gmail.com>
2025-10-01 17:15:45 +0200

    Added routes for UserList and UserDetails in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import UserList from "./components/UserList";
+import { Routes, Route } from "react-router-dom";
+import UserDetails from "./components/UserDetails";
 
 function App() {
   return (
     <div className="App">
       <h1>User Management App</h1>
-      <UserList />
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/user/:id" element={<UserDetails />} />
+      </Routes>
     </div>
   );
 }
